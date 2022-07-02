8db1876d06688566e50c74d7bf05ac6a44eaf5ed
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-07-03 01:17:56 +0530

    Create words.js

---
+++ words.js
@@ -0,0 +1,198 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "Programming language"
+    },
+    {
+        word: "guitar",
+        hint: "A musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "A purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "Planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "A yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "Online shopping site"
+    },
+    {
+        word: "golang",
+        hint: "Programming language"
+    },
+    {
+        word: "coding",
+        hint: "Related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "Science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "Related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "Epic science fiction film"
+    },
+    {
+        word: "gif",
+        hint: "A file format for image"
+    },
+    {
+        word: "mental",
+        hint: "Related to the mind"
+    },
+    {
+        word: "map",
+        hint: "Diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "Land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "A famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "Related to a indoor game"
+    },
+    {
+        word: "viber",
+        hint: "A social media app"
+    },
+    {
+        word: "github",
+        hint: "Code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "A image file format"
+    },
+    {
+        word: "silver",
+        hint: "Precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "An electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "Computer component"
+    },
+    {
+        word: "java",
+        hint: "Programming language"
+    },
+    {
+        word: "google",
+        hint: "Famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "Famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "Microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "A relational database system"
+    },
+    {
+        word: "nepal",
+        hint: "Developing country name"
+    },
+    {
+        word: "flute",
+        hint: "A musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "Related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "Unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "Planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "Related to server application"
+    },
+    {
+        word: "email",
+        hint: "Related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "Markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "Related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "A thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "Related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "A vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "A image file format"
+    },
+    {
+        word: "search",
+        hint: "Act to find something"
+    },
+    {
+        word: "key",
+        hint: "Small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "A country name"
+    },
+    {
+        word: "joker",
+        hint: "Psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "Developed country name"
+    },
+    {
+        word: "photo",
+        hint: "Representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "Largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "Related to a water"
+    },
+]
